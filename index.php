15a1916e642b1df839a99a4695d3bf913942a5be
HenryBenaye <henry.be@outlook.com>
2020-03-19 14:04:29 +0100

    Menu gemaakt

--- index.php
+++ index.php
@@ -7,12 +7,46 @@
 
 <body>
 
+    <style type="text/css">
+        #menu {
+            width: 70px;
+            height: 800px;
+            margin: 0px;
+            background-color: blue; 
+            display: flex;
+            justify-content: space-evenly;
+            flex-direction: column;
+            color: white;
+            align-items: center;
+
+        }
+
+        i {
+            color: white;
+            font-size: 30px;
+        }
+        
+    </style>
+
     <header></header>
 
-    <main></main>
+    <main>
+        <div id="menu">
+            <a href=""><i class="fas fa-home"></i></a>
+            <a href=""><i class="fas fa-satellite-dish"></i></a>
+            <a href=""><i class="fas fa-user"></i></a>
+            <a href=""><i class="fas fa-plus"></i></a>
+
+         </div>
+        </main>
+        
+        <footer></footer>
+        
+    </body>
+    <script src="https://kit.fontawesome.com/82664ff85a.js" crossorigin="anonymous"></script>
     
-    <footer></footer>
-
-</body>
-
-</html>
+    </html>
+    <!-- homepagina
+    community
+    profiel
+    post -->
