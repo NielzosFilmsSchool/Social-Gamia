5e2274f5e8be7e4f2522e29878717906e8ab31f1
HenryBenaye <henry.be@outlook.com>
2020-03-19 14:52:33 +0100

    2e menu done

---
+++ community_highlights.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="CSS/theme.css">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+    <div id='menu_2'>
+        <a href="">Highlights</i></a>
+        <a href="">Questions</i></a>
+        <a href="">Rules</i></a>
+        </div>
+    </header>
+
+    <main></main>
+    
+    <footer></footer>
+
+</body>
+<script src="https://kit.fontawesome.com/82664ff85a.js" crossorigin="anonymous"></script>
+
+</html>
